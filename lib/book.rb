004d6f7cb18124f3ef09c4ea0322bcfcd2441d76
Daniel <dannyb829@gmail.com>
2022-04-05 14:37:45 -0400

    commit tree 5f4c8ec6ea2043ebe88031c201303a02d2f36059

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
 
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+       @title = title
+
+    end
+
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+erotic_book = Book.new  "And Then There Were None"
 
